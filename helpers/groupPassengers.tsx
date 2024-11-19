b118703258d60df61ce67dc06195c39ee50f70ca
João Vitor Ribeiro <jvcarvalho09@hotmail.com>
2024-11-19 16:48:25 -0300

    update

--- helpers/groupPassengers.tsx
+++ helpers/groupPassengers.tsx
@@ -2,10 +2,8 @@
 import data from "../services/data";
 
 export const groupPassengersByCountry = () => {
-  // Usamos um objeto para acumular os países
-  const countryCount: { [key: string]: Set<number> } = {}; // Um Set para garantir passageiros únicos
+  const countryCount: { [key: string]: Set<number> } = {}; 
 
-  // Iteramos sobre os dados dos passageiros
   data.forEach((passenger) => {
     const countries = [passenger.origin, passenger.destination];
 
@@ -13,7 +11,7 @@ export const groupPassengersByCountry = () => {
       if (!countryCount[country]) {
         countryCount[country] = new Set();
       }
-      countryCount[country].add(passenger.id); // Adicionamos o ID do passageiro para contar apenas uma vez
+      countryCount[country].add(passenger.id); 
     });
   });
 
@@ -21,15 +19,12 @@ export const groupPassengersByCountry = () => {
     return count === 1 ? 'passageiro' : 'passageiros';
   };
 
-  // Transformamos o objeto em um array de seções, incluindo a contagem de passageiros
   const sections = Object.keys(countryCount).map((country) => ({
-    title: `${country} (${countryCount[country].size} ${formatPassengerCount(countryCount[country].size)})`, // Título da seção com a contagem
+    title: `${country} (${countryCount[country].size} ${formatPassengerCount(countryCount[country].size)})`, 
     data: data.filter((passenger) =>
-      // Filtra os passageiros que passaram pelo país
       passenger.origin === country || passenger.destination === country
     ),
   }));
 
-  // Ordenar as seções alfabeticamente com base no nome do país
   return sections.sort((a, b) => a.title.localeCompare(b.title));
 };
